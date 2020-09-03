9ce2dd0fe0a3dfabc04a6b271611fcf8156b32e5
Mary Baker <meb601@gmail.com>
2020-09-02 20:36:31 -0400

    Build out useDarkMode custom hook, import in index.js and replace useState with useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+// import { useLocalstorage } from './useLocalstorage';
+
+// export const useDarkMode = (key, initialValue) => {
+
+// const [values, setValues] = useLocalstorage(key, initialValue);
+
+// // const toggleDarkMode = e => {
+// //     if (style === "dark-mode App") {
+// //         return "!dark-mode App"
+// //     } else {
+// //         return "dark-mode App"
+// //     }
+// // };
+
+// return [values, toggleDarkMode];
+
+// };
