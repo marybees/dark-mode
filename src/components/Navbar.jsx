ebde70794ac30d8e5130ecc91f959abf450767d5
Mary Baker <meb601@gmail.com>
2020-09-02 20:58:33 -0400

    update useDarkMode import in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import useDarkMode from './hooks/useDarkMode'
+import useDarkMode from '../hooks/useDarkMode'
 
 const Navbar = (props) => {
   const [darkMode, setDarkMode] = useDarkMode();
