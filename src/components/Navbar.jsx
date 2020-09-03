555af32fbb4b4d6470aab836606fb3cc750bbc11
Mary Baker <meb601@gmail.com>
2020-09-02 20:39:47 -0400

    Update Navbar to include useDarkMode custom hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import useDarkMode from '../hooks/useDarkMode'
+import useDarkMode from './hooks/useDarkMode'
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode(false);
+const Navbar = (props) => {
+  const [darkMode, setDarkMode] = useDarkMode();
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
